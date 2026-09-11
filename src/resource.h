b86372719f40497ab1a65b1bcb10d49e5e1bc48b
AlexRed29 <alex2905mtz@gmail.com>
2026-09-11 09:26:33 -0600

    Update resource.h

--- src/resource.h
+++ src/resource.h
@@ -1,14 +1,17 @@
 //{{NO_DEPENDENCIES}}
 // Archivo de inclusión generado de Microsoft Visual C++.
-// Usado por GTA Symlink Generator.rc
+// Usado por GTASymlinkGenerator.rc
 //
-#define IDI_ICON1                       102
+#define IDI_ICON1                       101
+#define ENGLISH_INI    102
+#define SPANISH_INI    103
+#define PORTUGUESE_INI 104
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
